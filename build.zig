ea81f279a48ffb706f16c3ec07aff8c5188b888f
yu.otsubo <yu.otsubo@eventhub.jp>
2022-09-19 16:57:55 +0900

    release

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("zig_alerm", "src/main.zig");
+    const exe = b.addExecutable("zig_alarm", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
